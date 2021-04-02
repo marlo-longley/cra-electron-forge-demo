3258fd5928af6f93c973496877f82edc8a9d2f3f
Marlo Longley <marlo@codeandcursor.com>
2021-04-02 12:20:36 -0700

    Proof of concept linking with python script

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const { app, BrowserWindow } = require("electron");
+const { PythonShell } = require("python-shell");
 const isDev = require("electron-is-dev");
 
 // Conditionally include the dev tools installer to load React Dev Tools
@@ -17,7 +18,28 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
+
+const callPython = async () => {
+  let options = {
+    scriptPath : path.join(__dirname, '/python'),
+    args : [],
+    //mode: "json"
+  };
+
+  // call hellp.py
+  // wrap it in a promise, and `await` the result
+  const pyResult = await new Promise((resolve, reject) => {
+    PythonShell.run('hello.py', options, (err, results) => {
+      if (err) return reject(err);
+      return resolve(results);
+    });
+  });
+
+  return pyResult;
+};
+
 function createWindow() {
+
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
@@ -27,17 +49,30 @@ function createWindow() {
     }
   });
 
-  // and load the index.html of the app.
-  win.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
 
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: "detach" });
-  }
+  callPython().then(result => {
+    // append python data to query param
+    // after some quick searching this was the simplest way I could find to pass data to the BrowserWindow
+    // However it's obviously limited with how much and what you can pass. I'm not clear on what we'd want to pass from the grabbags. If it's a simple pass/fail variable then fine? But if it's a list of filenames this won't be sufficient.
+    // Also decoding the string on the React side would be easier with React Router, but I don't know if it's worth hooking that up.
+    // TODO: could look further into Electron stuff for passing data like IPC ?? Not familiar.
+    let queryString = `?pyResult=${result}`;
+
+    let url = isDev
+        ? `http://localhost:3000${queryString}`
+        : `file://${path.join(__dirname, "../build/index.html" + queryString)}`;
+
+    // and load the index.html of the app.
+    win.loadURL(url);
+    win.webContents.send('data', result);
+
+    // Open the DevTools.
+    if (isDev) {
+      win.webContents.openDevTools({ mode: "detach" });
+    }
+
+  });
+
 }
 
 // This method will be called when Electron has finished
@@ -48,8 +83,8 @@ app.whenReady().then(() => {
 
   if (isDev) {
     installExtension(REACT_DEVELOPER_TOOLS)
-      .then(name => console.log(`Added Extension:  ${name}`))
-      .catch(error => console.log(`An error occurred: , ${error}`));
+        .then(name => console.log(`Added Extension:  ${name}`))
+        .catch(error => console.log(`An error occurred: , ${error}`));
   }
 });
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Set initial state
+  // Currently ugly and not decoded
+  // Could wire up Router if we want to rely on query params or have multiple windows
+  const [data, setData] = useState(global.location.search);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Python Data from URL Param:</h2>
+          { data }
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
